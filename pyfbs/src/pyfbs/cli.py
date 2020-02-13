d61088ebc4a5b43e86ec0256885517e2ce26a3b0
mase5 <mdewaegeneer@gmail.com>
2020-02-13 22:04:39 +0100

    Init Python package using create-python-package

---
+++ pyfbs/src/pyfbs/cli.py
@@ -0,0 +1,17 @@
+"""Console script for pyfbs."""
+import argparse
+import sys
+def main():
+    """Console script for pyfbs."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "pyfbs.cli.main")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
---
+++ pyfbs/tests/test_pyfbs.py
@@ -0,0 +1,5 @@
+from pyfbs import pyfbs
+
+
+def test_pyfbs():
+    assert pyfbs is not None
