7500a87c50edba9bb75ef1ca4e619a72a3d96d6f
mase5 <mdewaegeneer@gmail.com>
2020-03-03 19:24:36 +0100

    Add helper functions for Coordinates2D  to get interested data out of fbs easily Requires flatbuffers

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+import { Dim } from './src/flatmake/idl/js/main_generated'
+import { flatbuffers } from 'flatbuffers'
+
+function toByteBuffer(bytes) {
+    const data = new Uint8Array(bytes);
+    return new flatbuffers.ByteBuffer(data);
+}
+
+function getCoordinates2D(bytes) {
+    const e = Dim.Coordinates2D.getRootAsCoordinates2D(
+        toByteBuffer(bytes),
+        null
+    );
+    return {
+        x: e.x().dataArray(),
+        y: e.y().dataArray()
+    }
+}
+
 module.exports = {
-    Dim: import('./src/flatmake/idl/js/main_generated'),
-};
+    getCoordinates2D
+}
