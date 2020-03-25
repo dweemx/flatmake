0fe3815c198a3f3800ffc4489986d2af8267295e
dweemx <mdewaegeneer@gmail.com>
2020-03-25 18:34:41 +0100

    Add install dependencies to setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,10 @@ setup(
     ],
     description="A variety of utilities to build high-level flatbuffers structures",
     entry_points={"console_scripts": ["flatmake=flatmake.cli:main", ], },
-    install_requires=[],
+    install_requires=[
+        'numpy==1.18.1',
+        'flatbuffers==1.12'
+    ],
     license="MIT license",
     long_description=readme,
     long_description_content_type="text/markdown",
